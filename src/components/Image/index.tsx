43f3646ea3cb02c58623776727bb31525cc9597b
TK <leandrotk100@gmail.com>
2020-05-02 12:34:30 -0300

    Fix image width and height

--- src/components/Image/index.tsx
+++ src/components/Image/index.tsx
@@ -1,28 +1,51 @@
-import React from 'react';
+import React, { CSSProperties } from 'react';
 import Skeleton from '@material-ui/lab/Skeleton';
 
 import { ProductType } from 'types/Product';
+import { imageWrapperStyle, imageStyle } from './styles';
 
 type ImageUrlType = Pick<ProductType, 'imageUrl'>;
 type ImageAttrType = { imageAlt: string; width?: string };
 type ImageStateType = { isLoading: boolean };
-type ImagePropsType = ImageUrlType & ImageAttrType & ImageStateType;
+type ImageStyleType = {
+  imageWrapperStyle: CSSProperties;
+  imageStyle: CSSProperties;
+};
+
+type ImagePropsType = ImageUrlType &
+  ImageAttrType &
+  ImageStateType &
+  ImageStyleType;
 
 export const Image = ({
   imageUrl,
   imageAlt,
   width,
   isLoading,
+  imageWrapperStyle,
+  imageStyle,
 }: ImagePropsType) => {
   if (isLoading) {
-    return <Skeleton variant="rect" width={width} height={167} />;
+    return (
+      <Skeleton
+        variant="rect"
+        width={width}
+        height={imageWrapperStyle.height}
+      />
+    );
   }
 
-  return <img src={imageUrl} alt={imageAlt} width={width} height={167} />;
+  return (
+    <div style={imageWrapperStyle}>
+      <img src={imageUrl} alt={imageAlt} width={width} style={imageStyle} />
+    </div>
+  );
 };
 
 Image.defaultProps = {
   width: '100%',
+  imageWrapperStyle,
+  imageStyle,
 };
 
 export default Image;
---
+++ src/components/Image/styles.ts
@@ -0,0 +1,11 @@
+import { CSSProperties } from 'react';
+
+export const imageWrapperStyle: CSSProperties = {
+  height: 170,
+};
+
+export const imageStyle: CSSProperties = {
+  height: '100%',
+  objectFit: 'cover',
+  borderRadius: '8px',
+};
